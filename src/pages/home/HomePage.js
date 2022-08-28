6d4513145417c4c9ae3440c1127317dda208480b
Luis Chota <65051397+luisftime24@users.noreply.github.com>
2022-08-27 21:05:17 -0500

    Add customers parameters and findByFieldAndValue method into homepage component

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -6,31 +6,58 @@ import Table from "../../components/Table";
 function HomePage() {
   const [customers, setCustomers] = useState([]);
   const [showCustomers, setShowCustomers] = useState([]);
-  const [city, setCity] = useState("");
+  const [field, setField] = useState("");
+  const [value, setValue] = useState("");
+
+  const customerParameters = [
+    "customerNumber",
+    "customerName",
+    "contactLastName",
+    "contactFirstName",
+    "phone",
+    "addressLine1",
+    "addressLine2",
+    "city",
+    "state",
+    "postalCode",
+    "country",
+    "salesRepEmployeeNumber",
+    "creditLimit",
+  ];
 
   useEffect(() => {
-    fetch(`http://localhost:8080/customers/${city}`)
+    fetch(`http://localhost:8080/customers`)
       .then((response) => response.json())
       .then((data) => {
         setCustomers(data);
         setShowCustomers(data);
       });
-  }, [city]);
+  }, [field]);
 
-  const getCustomersByCity = (city) => {
-    if (city === "") {
+  const filterCustomerByFieldAndValue = (field, value) => {
+    if (field === "") {
+      return customers;
+    }
+    if (value === "") {
       return [];
     }
-    return customers.filter((customer) => customer.city === city);
+    return customers.filter(
+      (customer) =>
+        customer[field].toString().toLowerCase().includes(value.toLowerCase())
+    );
   };
 
-  const handleChange = (event) => {
-    setCity(event.target.value);
+  const handleChangeField = (event) => {
+    setField(event.target.value);
+  };
+
+  const handleChangeValue = (event) => {
+    setValue(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setShowCustomers(getCustomersByCity(city));
+    setShowCustomers(filterCustomerByFieldAndValue(field, value));
   };
 
   return (
@@ -40,15 +67,42 @@ function HomePage() {
         <div className="row mt-4">
           <div className="col">
             <form onSubmit={handleSubmit}>
+              {/* Field */}
               <div className="row mb-3">
-                <label className="col-sm-3 col-form-label">ID:</label>
+                <label className="col-sm-3 col-form-label">Field:</label>
+                <div className="col-sm-9">
+                  <select className="form-select" value={field} onChange={handleChangeField}>
+                    <option defaultValue="">Select customer field</option>
+                    {customerParameters.map((param) => {
+                      return (
+                        <option key={param} value={param}>
+                          {param}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+              {/* Value */}
+              <div className="row mb-3">
+                <label className="col-sm-3 col-form-label">Value:</label>
                 <div className="col-sm-9">
                   <input
                     type="text"
+                    name="fieldValue"
                     className="form-control"
-                    placeholder="City"
-                    onChange={handleChange}
+                    placeholder="value"
+                    value={value}
+                    onChange={handleChangeValue}
                   />
+                </div>
+              </div>
+              <div className="row my-3">
+                <div className="col-sm-3"></div>
+                <div className="col-sm-9 d-grid">
+                  <button className="btn btn-success" type="submit">
+                    Search
+                  </button>
                 </div>
               </div>
             </form>
